c5c52f7c84c4e191aef547d28a5a94da1b913ff2
Cameron Morales <cam@Camerons-iMac.local>
2019-01-25 22:52:30 -0600

    feat: initial puzzle class layout

---
+++ EightPuzzle.py
@@ -0,0 +1,38 @@
+class EightPuzzle:
+    def __init__(self, start, goal, h):
+        self.start = start
+        self.goal = goal
+        self.h = h
+
+    def successor():
+        pass
+
+    def heuristic():
+        pass
+
+    def breadthFirst():
+        pass
+
+    def depthFirst():
+        pass
+
+    def iterativeDeepening():
+        pass
+
+    def uniformCost():
+        pass
+
+    def bestFirst():
+        pass
+
+    def A1():
+        pass
+
+    def A2():
+        pass
+
+    def A3():
+        pass
+
+    def printResults():
+        pass
